05099ab1eaae211adf3f536038fc17d0c4525760
kosar <salarikosar5@gmail.com>
2023-05-17 17:24:56 +0330

    calss Note complete

--- K4012/src/Note.java
+++ K4012/src/Note.java
@@ -1,4 +1,29 @@
 public class Note {
+
+    private int id;
+  
+    private String note;
+
+    public Note(int id, String note) {
+        this.id = id;
+        this.note = note;
+    } 
+
+     public Note() {
+    }
     
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
     
 }
