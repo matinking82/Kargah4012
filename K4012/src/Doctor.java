99c50cc3a482717b71bb262377492f9f45a0afae
kosar <salarikosar5@gmail.com>
2023-05-17 16:08:06 +0330

    class Doctor compelete

--- K4012/src/Doctor.java
+++ K4012/src/Doctor.java
@@ -1,3 +1,26 @@
 public class Doctor extends Personel{
+    private String Expertise;
+    
+
+    public Doctor() {
+        super();
+    }
+
+
+    public Doctor(String name, String phoneNumber, String emai, int age, int id, long salary, String shift,
+            boolean isAvalable, String username, String password, String expertise) {
+        super(name, phoneNumber, emai, age, id, salary, shift, isAvalable, username, password);
+        Expertise = expertise;
+    }
+    
+
+    public String getExpertise() {
+        return Expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        Expertise = expertise;
+    }
+
     
 }
