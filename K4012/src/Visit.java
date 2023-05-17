bc2092fa92f4483d82a63fb244d973cd76ff5967
matinking82 <Matinking82@gmail.com>
2023-05-17 16:34:51 +0330

    set relation note-visit

--- K4012/src/Visit.java
+++ K4012/src/Visit.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Visit {
-    
+    private List<Note> notes;
+
+    public Visit() {
+        notes = new ArrayList<Note>();
+    }
 }
