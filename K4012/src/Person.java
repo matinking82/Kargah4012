839c352a9b1cf0d441778e6ad38fdc9527554056
kosar <salarikosar5@gmail.com>
2023-05-17 15:50:00 +0330

    person class complete

--- K4012/src/Person.java
+++ K4012/src/Person.java
@@ -1,3 +1,58 @@
 public class Person {
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private int age;
+    private int id;
+
+    public Person(String name, String phoneNumber, String emai, int age, int id) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = emai;
+        this.age = age;
+        this.id = id;
+    } 
+
+    public Person() {
+
+    }
+    
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getEmai() {
+        return email;
+    }
+    public void setEmai(String emai) {
+        this.email = emai;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
     
 }
