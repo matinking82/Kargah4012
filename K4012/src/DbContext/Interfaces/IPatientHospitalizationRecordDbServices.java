82c25b6915b08da490c062a9df1d6690eb17a004
matinking82 <Matinking82@gmail.com>
2023-05-18 23:19:44 +0330

    complete patientHospitalizationRecordDbServices

--- K4012/src/DbContext/Interfaces/IPatientHospitalizationRecordDbServices.java
+++ K4012/src/DbContext/Interfaces/IPatientHospitalizationRecordDbServices.java
@@ -1,5 +1,14 @@
 package DbContext.Interfaces;
 
+import java.util.List;
+
+import Models.PatientHospitalizationRecord;
+
 public interface IPatientHospitalizationRecordDbServices {
-    
+    public List<PatientHospitalizationRecord> getAllPatientHospitalizationRecordsList();
+    public PatientHospitalizationRecord getById(int Id);
+    public boolean Add(PatientHospitalizationRecord patientHospitalizationRecord);
+    public boolean Remove(int patientHospitalizationRecordId);
+    public boolean Remove(PatientHospitalizationRecord patientHospitalizationRecord);
+    public boolean Update(PatientHospitalizationRecord patientHospitalizationRecord);
 }
--- K4012/src/DbContext/PatientHospitalizationRecordDbServices.java
+++ K4012/src/DbContext/PatientHospitalizationRecordDbServices.java
@@ -1,7 +1,149 @@
 package DbContext;
 
-import DbContext.Interfaces.IPatientHospitalizationRecordDbServices;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PatientHospitalizationRecordDbServices implements IPatientHospitalizationRecordDbServices{
-    
+import DbContext.Interfaces.IPatientHospitalizationRecordDbServices;
+import Models.PatientHospitalizationRecord;
+
+public class PatientHospitalizationRecordDbServices implements IPatientHospitalizationRecordDbServices {
+    private Connection connection;
+
+    public PatientHospitalizationRecordDbServices(Connection connection) {
+        this.connection = connection;
+    }
+
+    private int getLastId() {
+        int lastNurseId = -1;
+        try {
+            // Prepare the SQL statement
+            String sql = "SELECT MAX(id) AS MaxId FROM PatientHospitalizationRecord;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            // Execute the SQL statement
+            ResultSet rs = statement.executeQuery();
+
+            // Check if the result set has a row
+            if (rs.next()) {
+                // Get the value of the MaxId column
+                lastNurseId = rs.getInt("MaxId");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error getting last admin ID: " + e.getMessage());
+        }
+        return lastNurseId;
+    }
+
+    @Override
+    public List<PatientHospitalizationRecord> getAllPatientHospitalizationRecordsList() {
+        List<PatientHospitalizationRecord> patientHospitalizationRecords = new ArrayList<PatientHospitalizationRecord>();
+        try {
+            String sql = "SELECT * FROM PatientHospitalizationRecord";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                PatientHospitalizationRecord patientHospitalizationRecord = new PatientHospitalizationRecord();
+                patientHospitalizationRecord.setId(result.getInt("id"));
+                patientHospitalizationRecord.setDoctorId(result.getInt("doctorId"));
+                patientHospitalizationRecord.setPatientId(result.getInt("patientId"));
+                patientHospitalizationRecord.setHospitalizationPrice(result.getLong("hospitalizationPrice"));
+                patientHospitalizationRecord.setDate(result.getString("date"));
+                patientHospitalizationRecords.add(patientHospitalizationRecord);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving patient hospitalization records: " + e.getMessage());
+        }
+        return patientHospitalizationRecords;
+    }
+
+    @Override
+    public PatientHospitalizationRecord getById(int id) {
+        PatientHospitalizationRecord patientHospitalizationRecord = null;
+        try {
+            String sql = "SELECT * FROM PatientHospitalizationRecord WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                patientHospitalizationRecord = new PatientHospitalizationRecord();
+                patientHospitalizationRecord.setId(result.getInt("id"));
+                patientHospitalizationRecord.setDoctorId(result.getInt("doctorId"));
+                patientHospitalizationRecord.setPatientId(result.getInt("patientId"));
+                patientHospitalizationRecord.setHospitalizationPrice(result.getLong("hospitalizationPrice"));
+                patientHospitalizationRecord.setDate(result.getString("date"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving patient hospitalization record: " + e.getMessage());
+        }
+        return patientHospitalizationRecord;
+    }
+
+    @Override
+    public boolean Add(PatientHospitalizationRecord patientHospitalizationRecord) {
+        boolean success = false;
+        patientHospitalizationRecord.setId(getLastId() + 1);
+        try {
+            String sql = "INSERT INTO PatientHospitalizationRecord (id, doctorId, patientId, hospitalizationPrice, date) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, patientHospitalizationRecord.getId());
+            statement.setInt(2, patientHospitalizationRecord.getDoctorId());
+            statement.setInt(3, patientHospitalizationRecord.getPatientId());
+            statement.setLong(4, patientHospitalizationRecord.getHospitalizationPrice());
+            statement.setString(5, patientHospitalizationRecord.getDate());
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                success = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error adding patient hospitalization record: " + e.getMessage());
+        }
+        return success;
+    }
+
+    @Override
+    public boolean Remove(int patientHospitalizationRecordId) {
+        boolean success = false;
+        try {
+            String sql = "DELETE FROM PatientHospitalizationRecord WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, patientHospitalizationRecordId);
+            int rowsDeleted = statement.executeUpdate();
+            if (rowsDeleted > 0) {
+                success = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error deleting patient hospitalization record: " + e.getMessage());
+        }
+        return success;
+    }
+
+    @Override
+    public boolean Remove(PatientHospitalizationRecord patientHospitalizationRecord) {
+        return Remove(patientHospitalizationRecord.getId());
+    }
+
+    @Override
+    public boolean Update(PatientHospitalizationRecord patientHospitalizationRecord) {
+        boolean success = false;
+        try {
+            String sql = "UPDATE PatientHospitalizationRecord SET doctorId = ?, patientId = ?, hospitalizationPrice = ?, date = ? WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, patientHospitalizationRecord.getDoctorId());
+            statement.setInt(2, patientHospitalizationRecord.getPatientId());
+            statement.setLong(3, patientHospitalizationRecord.getHospitalizationPrice());
+            statement.setString(4, patientHospitalizationRecord.getDate());
+            statement.setInt(5, patientHospitalizationRecord.getId());
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                success = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error updating patient hospitalization record: " + e.getMessage());
+        }
+        return success;
+    }
 }
