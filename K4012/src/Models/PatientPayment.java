cc13e1ad13009c9fe6f4ea9df0052559a0694429
kosar <salarikosar5@gmail.com>
2023-05-18 18:10:46 +0330

    class PatientPayment complete

--- K4012/src/Models/PatientPayment.java
+++ K4012/src/Models/PatientPayment.java
@@ -2,5 +2,43 @@ package Models;
 
 public class PatientPayment {
     
+    private int patientId;
+    private int hospitalizationId;
+    private int visitId;
+
+    public PatientPayment(int patientId, int hospitalizationId, int visitId) {
+        this.patientId = patientId;
+        this.hospitalizationId = hospitalizationId;
+        this.visitId = visitId;
+    }
+
+    public PatientPayment()
+    {
+
+    }
+
+
+    public int getPatientId() {
+        return patientId;
+    }
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+   
+    public int getVisitId() {
+        return visitId;
+    }
+    public void setVisitId(int visitId) {
+        this.visitId = visitId;
+    }
+    
+    public int getHospitalizationId() {
+        return this.hospitalizationId;
+    }
+    public void setHospitalizationId(int hospitalizationId) {
+        this.hospitalizationId = hospitalizationId;
+    }
+    
+
     
 }
