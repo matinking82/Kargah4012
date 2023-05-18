4662aa1dffcefae5c586cb92ae412cd8d820c3f4
matinking82 <Matinking82@gmail.com>
2023-05-18 18:28:39 +0330

    add methods for adminDbServices

--- K4012/src/DbContext/Interfaces/IAdminDbServices.java
+++ K4012/src/DbContext/Interfaces/IAdminDbServices.java
@@ -1,5 +1,14 @@
 package DbContext.Interfaces;
 
+import java.util.List;
+
+import Models.Admin;
+
 public interface IAdminDbServices {
-    
+    public List<Admin> getAllAdminsList();
+    public Admin getById(int Id);
+    public boolean Add(Admin admin);
+    public boolean Remove(int adminId);
+    public boolean Remove(Admin admin);
+    public boolean Update(Admin admin);
 }
