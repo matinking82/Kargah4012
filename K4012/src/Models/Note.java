d711915fb9cf33f64248facb9857e923d9101de7
matinking82 <Matinking82@gmail.com>
2023-05-18 17:51:02 +0330

    add VisitId to Note

--- K4012/src/Models/Note.java
+++ K4012/src/Models/Note.java
@@ -2,17 +2,25 @@ package Models;
 public class Note {
 
     private int id;
-  
+    private int VisitId;
     private String note;
-
+    
+    
     public Note(int id, String note) {
         this.id = id;
         this.note = note;
     } 
-
-     public Note() {
+    
+    public Note() {
     }
     
+    public int getVisitId() {
+        return VisitId;
+    }
+
+    public void setVisitId(int visitId) {
+        VisitId = visitId;
+    }
     public int getId() {
         return id;
     }
