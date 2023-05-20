a9b046c713d1b974ab707bc8c03d23f577552c03
matinking82 <Matinking82@gmail.com>
2023-05-20 17:25:50 +0330

    add passwordHasher

--- K4012/src/Utils/PasswordHasher.java
+++ K4012/src/Utils/PasswordHasher.java
@@ -1,5 +1,25 @@
 package Utils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordHasher {
-    
+    public static String ToSha256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
