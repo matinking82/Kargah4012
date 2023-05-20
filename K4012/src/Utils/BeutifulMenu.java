2e85d725b890e5a6b8556322103f6cc86f37bf10
matinking82 <Matinking82@gmail.com>
2023-05-20 18:20:32 +0330

    complete showMenu

--- K4012/src/Utils/BeutifulMenu.java
+++ K4012/src/Utils/BeutifulMenu.java
@@ -5,25 +5,30 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.util.List;
+import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class BeutifulMenu {
     public static void showMenu(List<String> menu, MenuCallback callback) {
         // Create a new JFrame to display the menu
         JFrame frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(800, 600);
     
         // Create a new JPanel to hold the menu items
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(menu.size(), 1, 0, 10)); // Set the horizontal and vertical gaps
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Use a vertical BoxLayout to make the menu scrollable
         panel.setBackground(new Color(250, 250, 250));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area at the top of the panel
     
         // Add a custom JButton for each menu item
         for (int i = 0; i < menu.size(); i++) {
@@ -37,17 +42,26 @@ public class BeutifulMenu {
             button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
             button.setFont(new Font("Arial", Font.PLAIN, 16));
             button.setOpaque(true);
+            button.setPreferredSize(new Dimension(500, 50)); // Set a fixed height for the buttons
+            button.setMaximumSize(new Dimension(500, 50)); // Set the maximum size to the same fixed height
             button.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border radius
             button.setAlignmentX(Component.CENTER_ALIGNMENT); // Align the button in the center horizontally
             panel.add(button);
+            panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area below each button
             button.addActionListener(e -> {
                 frame.dispose(); // Close the JFrame
                 callback.onMenuSelected(index + 1);
             });
         }
     
-        // Add the JPanel to the JFrame and show it
-        frame.add(panel);
+        // Create a new JScrollPane to make the menu scrollable
+        JScrollPane scrollPane = new JScrollPane(panel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(600, 400));
+    
+        // Add the JScrollPane to the JFrame and show it
+        frame.add(scrollPane);
         frame.setVisible(true);
     }
     
