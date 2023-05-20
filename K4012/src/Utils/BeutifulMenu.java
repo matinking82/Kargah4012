974543be987f42741ac56d5118937ae9a6ce426d
matinking82 <Matinking82@gmail.com>
2023-05-20 17:16:29 +0330

    showMenu completed

--- K4012/src/Utils/BeutifulMenu.java
+++ K4012/src/Utils/BeutifulMenu.java
@@ -2,6 +2,7 @@ package Utils;
 
 import java.awt.GridLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.util.List;
 
@@ -20,30 +21,29 @@ public class BeutifulMenu {
     
         // Create a new JPanel to hold the menu items
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(menu.size(), 1));
+        panel.setLayout(new GridLayout(menu.size(), 1, 0, 10)); // Set the horizontal and vertical gaps
         panel.setBackground(new Color(250, 250, 250));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     
-        // Add a custom JButton and JLabel for each menu item
+        // Add a custom JButton for each menu item
         for (int i = 0; i < menu.size(); i++) {
             final int index = i; // Create a final variable to hold the value of i
             String menuItem = menu.get(i);
-            JLabel label = new JLabel(menuItem);
-            label.setFont(new Font("Arial", Font.PLAIN, 16));
-            JButton button = new JButton(menuItem);
+            JButton button = new JButton();
+            button.setText(menuItem);
             button.setBackground(new Color(240, 240, 240));
-           // Set custom styles for the JButton
-            button.setOpaque(true);
-            button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-            button.setFont(new Font("Arial", Font.PLAIN, 16));
             button.setForeground(new Color(60, 60, 60));
             button.setFocusPainted(false);
+            button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+            button.setFont(new Font("Arial", Font.PLAIN, 16));
+            button.setOpaque(true);
+            button.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border radius
+            button.setAlignmentX(Component.CENTER_ALIGNMENT); // Align the button in the center horizontally
+            panel.add(button);
             button.addActionListener(e -> {
                 frame.dispose(); // Close the JFrame
                 callback.onMenuSelected(index + 1);
             });
-            panel.add(label);
-            panel.add(button);
         }
     
         // Add the JPanel to the JFrame and show it
