c65131eeb015a3c413a6079ee20d2670a388192f
matinking82 <Matinking82@gmail.com>
2023-05-22 17:30:44 +0330

    add showlist and get methods for patient

--- K4012/src/Utils/BeutifulMenu.java
+++ K4012/src/Utils/BeutifulMenu.java
@@ -20,6 +20,7 @@ import javax.swing.JScrollPane;
 
 import Models.Doctor;
 import Models.Nurse;
+import Models.Patient;
 import Models.Personel;
 
 public class BeutifulMenu {
@@ -464,6 +465,132 @@ public class BeutifulMenu {
         frame.setVisible(true);
     }
 
+    public static void showPatientsList(List<Patient> patientsList, String title, ListCallback callback) {
+        // Create a new JFrame to display the list of doctors
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Width, Height);
+
+        // Create a new JPanel to hold the list of doctors
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Use a vertical BoxLayout to make the list scrollable
+        panel.setBackground(new Color(250, 250, 250));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area at the top of the panel
+
+        // Create a new JPanel to hold the selected doctor's information
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.setBackground(new Color(250, 250, 250));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        infoPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigidarea at the top of the panel
+
+        // Add a custom JButton for each doctor
+        for (int i = 0; i < patientsList.size(); i++) {
+            final int index = i; // Create a final variable to hold the value of i
+            Patient patient = patientsList.get(i);
+            JButton button = new JButton();
+            button.setText(patient.getName());
+            button.setBackground(new Color(240, 240, 240));
+            button.setForeground(new Color(60, 60, 60));
+            button.setFocusPainted(false);
+            button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+            button.setFont(new Font("Arial", Font.PLAIN, 16));
+            button.setOpaque(true);
+            button.setPreferredSize(new Dimension(500, 80)); // Set a fixed size for the buttons
+            button.setMaximumSize(new Dimension(600, 100)); // Set the maximum size to the same fixed size
+            button.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border radius
+            button.setAlignmentX(Component.CENTER_ALIGNMENT); // Align the button in the center horizontally
+            panel.add(button);
+            panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area below each button
+            button.addActionListener(e -> {
+                // Clear the infoPanel to remove any previously selected doctor's information
+                infoPanel.removeAll();
+                infoPanel.revalidate();
+                infoPanel.repaint();
+
+                // Add the selected doctor's information to the infoPanel
+                JLabel nameLabel = new JLabel(
+                        "Name: " + (patient.getGender() == "male" ? "mr " : "mrs ") + patient.getName());
+                nameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(nameLabel);
+
+                JLabel emailLabel = new JLabel("email: " + patient.getEmail());
+                emailLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(emailLabel);
+
+                JLabel phoneLabel = new JLabel("phone: " + patient.getPhoneNumber());
+                phoneLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(phoneLabel);
+                
+                JLabel statusLabel = new JLabel("insurance: " + (patient.isHaveInsured() ? "Available" : "Not available"));
+                statusLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(statusLabel);
+                
+                JLabel descriptionLabel = new JLabel("description: " + patient.getDescription());
+                descriptionLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(descriptionLabel);
+
+                JButton btnSelect = new JButton("Select");
+                btnSelect.addActionListener(ee -> {
+                    frame.dispose(); // Close the JFrame
+                    callback.onItemSelected(patient.getId());
+                });
+                infoPanel.add(btnSelect);
+
+                // Repaint the infoPanel to display the selected doctor's information
+                infoPanel.revalidate();
+                infoPanel.repaint();
+            });
+        }
+
+        // Create a new JPanel to hold the return button
+        JPanel returnPanel = new JPanel();
+        returnPanel.setLayout(new BoxLayout(returnPanel, BoxLayout.X_AXIS));
+        returnPanel.setBackground(new Color(250, 250, 250));
+        returnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        returnPanel.add(Box.createHorizontalGlue()); // Add a horizontal glue to the left of the return button to keep
+                                                     // it fixed to the right
+        JButton returnButton = new JButton("Return");
+        returnButton.setBackground(new Color(240, 240, 240));
+        returnButton.setForeground(new Color(60, 60, 60));
+        returnButton.setFocusPainted(false);
+        returnButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        returnButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        returnButton.setOpaque(true);
+        returnButton.setPreferredSize(new Dimension(100, 50)); // Set a fixed size for the button
+        returnButton.setMaximumSize(new Dimension(100, 50)); // Set the maximum size to the same fixed size
+        returnButton.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border
+                                                                                                   // radius
+        returnButton.setAlignmentX(Component.RIGHT_ALIGNMENT); // Align the button to the right horizontally
+        returnPanel.add(returnButton);
+
+        // Add the return button to the bottom of the frame
+        frame.add(returnPanel, BorderLayout.SOUTH);
+
+        // Create a new JScrollPane to make the list of doctors scrollable
+        JScrollPane scrollPane = new JScrollPane(panel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(600, 400));
+
+        // Add the JScrollPane to the left side of the frame
+        frame.add(scrollPane, BorderLayout.WEST);
+
+        // Add the infoPanel to the right side of the frame
+        frame.add(infoPanel, BorderLayout.CENTER);
+
+        // Add an action listener to the return button to close the JFrame and call the
+        // callback
+        returnButton.addActionListener(e -> {
+            frame.dispose(); // Close the JFrame
+            callback.onReturn();
+        });
+
+        // Show the JFrame
+        frame.setVisible(true);
+    }
+
     public static void getDoctorFromUser(CreateDoctorCallBack createDoctorCallBack) {
         // Create a new JFrame to hold the input fields
         JFrame frame = new JFrame("Doctor Information");
@@ -773,7 +900,7 @@ public class BeutifulMenu {
 
     public static void getPersonelFromUser(CreatePersonelCallBack createPersonelCallBack) {
         // Create a new JFrame to hold the input fields
-        JFrame frame = new JFrame("Nurse Information");
+        JFrame frame = new JFrame("Personel Information");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(Width, Height);
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
@@ -908,9 +1035,117 @@ public class BeutifulMenu {
         frame.setVisible(true);
     }
 
+    public static void getPatientFromUser(CreatePatientCallBack createPatientCallBack) {
+        // Create a new JFrame to hold the input fields
+        JFrame frame = new JFrame("Patient Information");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Width, Height);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+    
+        // Create the input fields and labels
+        JTextField nameField = new JTextField();
+        nameField.setPreferredSize(new Dimension(300, nameField.getPreferredSize().height));
+        JLabel nameLabel = new JLabel("Name:");
+        JPanel namePanel = new JPanel();
+        namePanel.add(nameLabel);
+        namePanel.add(nameField);
+    
+        JTextField genderField = new JTextField();
+        genderField.setPreferredSize(new Dimension(300, genderField.getPreferredSize().height));
+        JLabel genderLabel = new JLabel("Gender:");
+        JPanel genderPanel = new JPanel();
+        genderPanel.add(genderLabel);
+        genderPanel.add(genderField);
+    
+        JTextField phoneNumberField = new JTextField();
+        phoneNumberField.setPreferredSize(new Dimension(300, phoneNumberField.getPreferredSize().height));
+        JLabel phoneNumberLabel = new JLabel("Phone Number:");
+        JPanel phoneNumberPanel = new JPanel();
+        phoneNumberPanel.add(phoneNumberLabel);
+        phoneNumberPanel.add(phoneNumberField);
+    
+        JTextField emailField = new JTextField();
+        emailField.setPreferredSize(new Dimension(300, emailField.getPreferredSize().height));
+        JLabel emailLabel = new JLabel("Email:");
+        JPanel emailPanel = new JPanel();
+        emailPanel.add(emailLabel);
+        emailPanel.add(emailField);
+    
+        JTextField ageField = new JTextField();
+        ageField.setPreferredSize(new Dimension(300, ageField.getPreferredSize().height));
+        JLabel ageLabel = new JLabel("Age:");
+        JPanel agePanel = new JPanel();
+        agePanel.add(ageLabel);
+        agePanel.add(ageField);
+
+        JTextField descriptionField = new JTextField();
+        descriptionField.setPreferredSize(new Dimension(300, descriptionField.getPreferredSize().height));
+        JLabel descriptionLabel = new JLabel("Email:");
+        JPanel descriptionPanel = new JPanel();
+        descriptionPanel.add(descriptionLabel);
+        descriptionPanel.add(descriptionField);
+
+        // Add the input fields and labels to the JFrame
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(namePanel);
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(genderPanel);
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(phoneNumberPanel);
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(descriptionPanel);
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(agePanel);
+        frame.add(Box.createVerticalStrut(10));
+        frame.add(descriptionPanel);
+    
+        // Create a button for the user to submit their input
+        JButton submitButton= new JButton("Submit");
+        submitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get the input values from the fields
+                String name = nameField.getText();
+                String gender = genderField.getText();
+                String phoneNumber = phoneNumberField.getText();
+                String email = emailField.getText();
+                int age = Integer.parseInt(ageField.getText());
+                String description = descriptionField.getText();
+    
+                // Create a new Doctor object with the input values
+                Patient patient = new Patient();
+                patient.setName(name);
+                patient.setGender(gender);
+                patient.setPhoneNumber(phoneNumber);
+                patient.setEmail(email);
+                patient.setAge(age);
+                patient.setDescription(description);
+    
+                // Close the JFrame
+                frame.dispose();
+    
+                // Call the callback interface with the created Doctor object
+                createPatientCallBack.onPatientCreated(patient);
+            }
+        });
+    
+        // Add the submit button to the JFrame
+        frame.add(Box.createVerticalStrut(20));
+        frame.add(submitButton);
+        frame.add(Box.createVerticalStrut(10));
+    
+        // Set the JFrame to visible
+        frame.setVisible(true);
+    }
+
 
     public interface CreateDoctorCallBack{
         public void onDoctorCreated(Doctor doctor);
+    }
+
+    public interface CreatePatientCallBack{
+        public void onPatientCreated(Patient patient);
     }
 
     public interface CreateNurseCallBack{
