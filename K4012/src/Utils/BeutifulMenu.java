c44e5f22dc147ba4538d08a0e0c61c29e71f9e35
matinking82 <Matinking82@gmail.com>
2023-05-27 17:37:42 +0330

    add visit selection method

--- K4012/src/Utils/BeutifulMenu.java
+++ K4012/src/Utils/BeutifulMenu.java
@@ -719,6 +719,129 @@ public class BeutifulMenu {
         frame.setVisible(true);
     }
 
+    public static void showVisitsListForSelection(List<Visit> visitsList, String title, ListCallback callback) {
+        // Create a new JFrame to display the list of doctors
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Width, Height);
+
+        // Create a new JPanel to hold the list of doctors
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Use a vertical BoxLayout to make the list scrollable
+        panel.setBackground(new Color(250, 250, 250));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area at the top of the panel
+
+        // Create a new JPanel to hold the selected doctor's information
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.setBackground(new Color(250, 250, 250));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        infoPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigidarea at the top of the panel
+
+        // Add a custom JButton for each doctor
+        for (int i = 0; i < visitsList.size(); i++) {
+            final int index = i; // Create a final variable to hold the value of i
+            Visit visit = visitsList.get(i);
+            JButton button = new JButton();
+
+            Doctor doctor = context.Doctors().getById(visit.getDoctorId());
+            Patient patient = context.Patients().getById(visit.getPatientId());
+
+            button.setText(doctor.getName() + " - " + patient.getName());
+            button.setBackground(new Color(240, 240, 240));
+            button.setForeground(new Color(60, 60, 60));
+            button.setFocusPainted(false);
+            button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+            button.setFont(new Font("Arial", Font.PLAIN, 16));
+            button.setOpaque(true);
+            button.setPreferredSize(new Dimension(500, 80)); // Set a fixed size for the buttons
+            button.setMaximumSize(new Dimension(600, 100)); // Set the maximum size to the same fixed size
+            button.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border radius
+            button.setAlignmentX(Component.CENTER_ALIGNMENT); // Align the button in the center horizontally
+            panel.add(button);
+            panel.add(Box.createRigidArea(new Dimension(0, 20))); // Add a rigid area below each button
+            button.addActionListener(e -> {
+                // Clear the infoPanel to remove any previously selected doctor's information
+                infoPanel.removeAll();
+                infoPanel.revalidate();
+                infoPanel.repaint();
+
+                // Add the selected doctor's information to the infoPanel
+                JLabel doctorNameLabel = new JLabel(
+                        "Doctor Name: " + (doctor.getGender() == "male" ? "mr " : "mrs ") + doctor.getName());
+                doctorNameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(doctorNameLabel);
+
+                JLabel patientNameLabel = new JLabel(
+                        "Patient Name: " + (patient.getGender() == "male" ? "mr " : "mrs ") + patient.getName());
+                patientNameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(patientNameLabel);
+
+                JLabel dateLabel = new JLabel("date of visit: " + visit.getDate());
+                dateLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+                infoPanel.add(dateLabel);
+
+                JButton btnSelect = new JButton("Select");
+                btnSelect.addActionListener(ee -> {
+                    frame.dispose(); // Close the JFrame
+                    callback.onItemSelected(visit.getId());
+                });
+                infoPanel.add(btnSelect);
+
+                // Repaint the infoPanel to display the selected doctor's information
+                infoPanel.revalidate();
+                infoPanel.repaint();
+            });
+        }
+
+        // Create a new JPanel to hold the return button
+        JPanel returnPanel = new JPanel();
+        returnPanel.setLayout(new BoxLayout(returnPanel, BoxLayout.X_AXIS));
+        returnPanel.setBackground(new Color(250, 250, 250));
+        returnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        returnPanel.add(Box.createHorizontalGlue()); // Add a horizontal glue to the left of the return button to keep
+                                                     // it fixed to the right
+        JButton returnButton = new JButton("Return");
+        returnButton.setBackground(new Color(240, 240, 240));
+        returnButton.setForeground(new Color(60, 60, 60));
+        returnButton.setFocusPainted(false);
+        returnButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        returnButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        returnButton.setOpaque(true);
+        returnButton.setPreferredSize(new Dimension(100, 50)); // Set a fixed size for the button
+        returnButton.setMaximumSize(new Dimension(100, 50)); // Set the maximum size to the same fixed size
+        returnButton.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 5, true)); // Set a border
+                                                                                                   // radius
+        returnButton.setAlignmentX(Component.RIGHT_ALIGNMENT); // Align the button to the right horizontally
+        returnPanel.add(returnButton);
+
+        // Add the return button to the bottom of the frame
+        frame.add(returnPanel, BorderLayout.SOUTH);
+
+        // Create a new JScrollPane to make the list of doctors scrollable
+        JScrollPane scrollPane = new JScrollPane(panel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(600, 400));
+
+        // Add the JScrollPane to the left side of the frame
+        frame.add(scrollPane, BorderLayout.WEST);
+
+        // Add the infoPanel to the right side of the frame
+        frame.add(infoPanel, BorderLayout.CENTER);
+
+        // Add an action listener to the return button to close the JFrame and call the
+        // callback
+        returnButton.addActionListener(e -> {
+            frame.dispose(); // Close the JFrame
+            callback.onReturn();
+        });
+
+        // Show the JFrame
+        frame.setVisible(true);
+    }
+
     public static void showHospitalizationList(List<PatientHospitalizationRecord> hospitalizationList, String title,
             ListCallback callback) {
         // Create a new JFrame to display the list of doctors
