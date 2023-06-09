c167f81f0f5e093ab870226d0f7c9627d7c9f78a
matinking82 <Matinking82@gmail.com>
2023-06-09 18:18:21 +0330

    add reset method

--- K4012/src/Utils/LoginInfo.java
+++ K4012/src/Utils/LoginInfo.java
@@ -3,9 +3,15 @@ package Utils;
 public class LoginInfo {
     public static boolean IsLogged = false;
     public static int LoginId;
-    public static loginType type; 
+    public static loginType type;
 
-    public enum loginType{
+    public static void reset() {
+        IsLogged = false;
+        LoginId = 0;
+        type = null;
+    }
+
+    public enum loginType {
         Admin,
         Nurse,
         Doctor,
